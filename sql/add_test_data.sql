660884ae20012f469c5614603c84eb3479a28a32
Leevi Heino <leeviheino@live.fi>
2016-12-01 23:19:44 +0200

    Tietokantadataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,8 +1,23 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+/* Set categories */
 
+INSERT INTO Category (title) VALUES ('CS');
 
-INSERT INTO Memo (title, content, priority) VALUES ('Rästit', 'Tee rästit', 9001);
+INSERT INTO Category (title) VALUES ('Kiireelliset');
 
-INSERT INTO Category (title) VALUES ('CS'); 
+INSERT INTO Category (title) VALUES ('Ei-kiireelliset');
+
+INSERT INTO Category (title) VALUES ('Sekalaiset');
+
+INSERT INTO Category (title) VALUES ('Jonninjoutavat');
+
+INSERT INTO Category (title) VALUES ('Tärkeät');
+
+INSERT INTO Category (title) VALUES ('Tietokantasovelluksen TODO-lista');
+
+INSERT INTO Category (title) VALUES ('Kissat');
+
+/* Set users */
 
 INSERT INTO UserAccount (username, password) VALUES ('Linus', '1234');
+
+INSERT INTO UserAccount (username, password) VALUES ('Turing', '4321');
