7574fc5eaf5378f1de8dc796e2d91f46aa8a2418
Leevi Heino <leeviheino@live.fi>
2016-12-01 17:12:25 +0200

    Reitit korjattu

--- config/routes.php
+++ config/routes.php
@@ -1,55 +1,55 @@
 <?php
 
-$routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() { // sandbox
 
     MemoController::sandbox();
 });
 
-$routes->get('/', function() {
+$routes->get('/', function() { // home page
 
     MemoController::index();
 });
 
-$routes->get('/categories', function() {
+$routes->get('/categories', function() { // categories page
 
     MemoController::categories();
 });
 
-$routes->post('/', function() {
+$routes->post('/', function() { // add new memo
 
     MemoController::store();
 });
 
-$routes->get('/new', function() {
+$routes->get('/new', function() { // new memo page
 
     MemoController::create();
 });
 
-$routes->get('/:id', function($id) {
+$routes->get('/edit/:id', function($id) { // edit memo page
 
     MemoController::edit($id);
 });
 
-$routes->post('/:id', function() {
+$routes->post('/edit/:id', function() { // update existing memo
    
     MemoController::update();
 });
 
-$routes->get('/delete/:id', function($id) {
+$routes->get('/delete/:id', function($id) { // delete memo
 
     MemoController::delete($id);
 });
 
-$routes->get('/show/:id', function($id) {
+$routes->get('/show/:id', function($id) { // show memo page
    
     MemoController::show($id);
 });
 
-$routes->get('/login', function(){
+$routes->get('/login', function() { // login page
 
     UserController::login();
 });
-$routes->post('/login', function(){
+$routes->post('/login', function() { // log user in
 
     UserController::handle_login();
 });
