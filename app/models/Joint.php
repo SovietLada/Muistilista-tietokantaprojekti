72ac3c88c3ef7e69f77b2264992a9948a25524ea
Heino Leevi <leevihei@da2-cs-z1-01.helsinki.fi>
2016-12-05 11:30:38 +0200

    Joint dataobjekti

---
+++ app/models/Joint.php
@@ -0,0 +1,22 @@
+<?php
+
+class Joint extends BaseModel {
+
+    public $id, $memoRef, $categoryRef;
+
+    public function __construct($attributes) {
+
+        parent::__construct($attributes);
+    }
+    
+    public function all() {
+        
+        // TODO: implementation
+    }
+    
+    public function find($id) {
+        
+        // TODO: implementation
+    }
+
+}
