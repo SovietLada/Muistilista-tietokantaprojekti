ee5731c0b9c1ab3ce3999ff8478daa51a29b6a17
Heino Leevi <leevihei@da2-cs-z1-01.helsinki.fi>
2016-12-05 10:58:25 +0200

    lisäyslogiikkaa

--- app/controllers/memos_controller.php
+++ app/controllers/memos_controller.php
@@ -1,7 +1,7 @@
 <?php
 
 class MemoController extends BaseController {
-    
+
     public static function index() {
 
         $memos = Memo::all(); // get memos
@@ -15,7 +15,7 @@ class MemoController extends BaseController {
     }
 
     public static function create() {
-
+        
         View::make('memo_new.html');
     }
 
@@ -31,7 +31,8 @@ class MemoController extends BaseController {
 
         $errors = $memo->validateParams();
         if (count($errors) > 0) {
-            Redirect::to('/', array('errors' => $errors));
+            View::make('memo_new.html', array('errors' => $errors));
+            // View::make('memo_new.html', array('attributes' => $memo));
         }
 
         $memo->save();
@@ -43,34 +44,35 @@ class MemoController extends BaseController {
         $memo = Memo::find($id);
         View::make('memo_edit.html', array('attributes' => $memo));
     }
-    
+
     public static function update() {
-        
+
         $params = $_POST; // post params from edit view
         $memo = Memo::find($params['id']);
         $memo->title = $params['title'];
         $memo->content = $params['content'];
         $memo->priority = $params['priority'];
-        
+
         $errors = $memo->validateParams();
         if (count($errors) > 0) {
             Redirect::to('/edit/' . $params['id'], array('errors' => $errors));
         }
-        
+
         $memo->update();
         Redirect::to('/show/' . $memo->id, array('success' => 'Muokkaus onnistui'));
     }
-    
+
     public static function delete($id) {
-        
+
         $memo = Memo::find($id);
         $memo->delete();
         Redirect::to('/', array('success' => 'Poisto onnistui'));
     }
-    
+
     public static function show($id) {
-        
+
         $memo = Memo::find($id);
         View::make('memo_show.html', array('attributes' => $memo));
     }
+
 }
